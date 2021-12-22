32b8768604c47da9e55900f19a74f0520611aea1
“K-takashii” <nanase.sigre.1627.yorushika@gmail.com>
2021-12-22 20:13:49 +0900

    Initial commit

---
+++ Equivalence.java
@@ -0,0 +1,9 @@
+class Equivalence {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 2;
+
+        System.out.println(x == y);
+        System.out.println(x != y);
+    }
+}
